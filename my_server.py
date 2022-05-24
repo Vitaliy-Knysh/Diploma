74018eabd4e5886efcab22340cbebbeedbf07ae8
Vitaliy-Knysh <75195148+Vitaliy-Knysh@users.noreply.github.com>
2022-05-24 18:27:38 +0300

    рабочий код сервера

--- my_server.py
+++ my_server.py
@@ -1,19 +1,40 @@
 import socket
 
-def connect():
-    server = socket.socket()
+command = 'l'
+serverReadyFlag = False
+
+
+def start():
+    global serverReadyFlag
+    global command
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     IP = socket.gethostbyname(socket.gethostname())
     PORT = 1034
     server.bind((IP, PORT))
     server.listen(1)
     usr, addr = server.accept()
-    return usr, addr
+    print('connected at ', usr)
+    while True:
+        data = usr.recv(400).decode('utf-8')
+        print('data: ', data)
+        if data:
+            if data == 'r':
+                print('ready flag received')
+                usr.send(command.encode('utf-8'))
+                print('command sent: ', command)
+                command = 's'
 
-def send(usr, char):
-    usr.send(char.encode('utf-8'))
 
-def listen(usr):
-    try: data = usr.recv(1024).decode('utf-8')
-    except socket.error:
-        pass
-    else: return data
+def handle_client(usr):
+    data = usr.recv(400).decode('utf-8')
+    global serverReadyFlag
+    if data:
+        if data == 'r' and serverReadyFlag == True:
+            usr.send(command.encode('utf-8'))
+            serverReadyFlag = False
+    else: return 'n'
+
+'''    while True:
+        #if listen(usr) == 'r' and serverReadyFlag == True:
+        send(usr, command)
+        serverReadyFlag = False'''
