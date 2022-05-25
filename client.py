4119110f6f7c080bb212102dd7c3a49920f22e7a
Vitaliy-Knysh <75195148+Vitaliy-Knysh@users.noreply.github.com>
2022-05-25 17:46:03 +0300

    Клиентское приложение которое отправляет данные с лидара

--- client.py
+++ client.py
@@ -1,7 +1,11 @@
 from jetbot import Robot
 import socket
 import time
+from rplidar import RPLidar
+import pickle
 
+lidar = RPLidar('/dev/ttyUSB0')
+arr = []
 leftMotor = rightMotor = moveTime = 0
 PORT = 1034
 FORMAT = 'utf-8'
@@ -12,6 +16,7 @@ robot = Robot()
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 print('connected')
+lidar.start_motor()
 
 
 def send(msg):
@@ -21,26 +26,48 @@ def send(msg):
     
 send('r')
 while True:
+    data = ''
     data = client.recv(20).decode(FORMAT)
     if data:
         if data == 'f':
-            leftMotor = rightMotor = 0.5
-            moveTime = 0.5
+            leftMotor = rightMotor = -0.5
+            moveTime = 0.15
         elif data == 'l':
             leftMotor = 0
-            rightMotor = 0.5
+            rightMotor = -0.5
             moveTime = 0.15
         elif data == 'r':
-            leftMotor = 0.5
+            leftMotor = -0.5
             rightMotor = 0
             moveTime = 0.15
         elif data == 's':
             leftMotor = 0
             rightMotor = 0
             moveTime = 1
+        elif data == 'm':  # получение данных с лидара
+            lidar.start_motor()
+            data = ''
+            robot.set_motors(0, 0)
+            time.sleep(1)
+            leftMotor = 0
+            rightMotor = 0
+            moveTime = 0
+            for i, scan in enumerate(lidar.iter_scans(scan_type='express')):
+                print('%d: Got %d measurments' % (i, len(scan)))
+                if len(scan) > 100:
+                    arr = scan
+                if i >= 2:
+                    break
+            for i in arr:
+                data += str(round(i[1], 1)) + ' ' + str(round(i[2])) + ' '
+            send(data)
+            lidar.stop()
+
+            
         robot.set_motors(leftMotor, rightMotor)
         time.sleep(moveTime)
         robot.set_motors(0, 0)
+        time.sleep(0.3)
         print(data)
         send('r')
         if data == 'c':
